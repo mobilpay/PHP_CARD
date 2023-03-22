b1918955bbcdf89557e9a81a3591d88349f9d597
andreipanait <andrei@netopia.ro>
2023-03-22 12:31:05 +0200

    OpenSSL 3+ update

--- Mobilpay/Payment/Request/Abstract.php
+++ Mobilpay/Payment/Request/Abstract.php
@@ -38,6 +38,8 @@ abstract class Mobilpay_Payment_Request_Abstract
 	const ERROR_CONFIRM_INVALID_POST_METHOD					= 0x300000f4;
 	const ERROR_CONFIRM_INVALID_POST_PARAMETERS				= 0x300000f5;
 	const ERROR_CONFIRM_INVALID_ACTION						= 0x300000f6;
+	const ERROR_CONFIRM_FAILED_DECODING_IV 					= 0x300000f7;
+	const ERROR_REQUIRED_CIPHER_NOT_AVAILABLE				= 0x300000f8;
 
 	const VERSION_QUERY_STRING	= 0x01;
     const VERSION_XML			= 0x02;	
@@ -86,6 +88,12 @@ abstract class Mobilpay_Payment_Request_Abstract
 	 * @var string
 	 */
 	public $confirmUrl 	= null;
+	
+	/**
+	 * @var string
+	 */
+	public $ipnCipher = null;
+
 
 	public $params		= array();
 	
@@ -93,13 +101,25 @@ abstract class Mobilpay_Payment_Request_Abstract
 	 * outEnvKey - output envelope key
 	 * in this property is stored the envelope key after encrypting data to send to payment interface 
 	 */
-	private $outEnvKey	= null;
+	protected $outEnvKey	= null;
 	
 	/**
 	 * outEncData - output encrypted data
 	 * in this property is stored the encrypted data to send to payment interface 
 	 */
-	private $outEncData	= null;
+	protected $outEncData	= null;	
+	
+	/**
+	 * cipher algorithm used to encrypt data
+	 * @var string
+	 */
+	protected $outCipher = null;
+	
+	/**
+	 * intialization vector seed used to encrypt data
+	 * @var string
+	 */
+	protected $outIv = null;
 	
 	protected $_xmlDoc	= null;
 	
@@ -109,13 +129,20 @@ abstract class Mobilpay_Payment_Request_Abstract
 	protected $_objRequestInfo	= null;
 	
 	public $objReqNotify		= null;
+	public $selectedInstallments 	= null;
+	public $reqInstallments 		= null;
+	public $secretCode 				= null;
+	public $paymentInstrument 		= null;
 	
+	/**
+	 *
+	 * Constructor
+	 */	
 	public function __construct()
 	{
-		srand((double) microtime() * 1000000);
-        $this->_requestIdentifier = md5(uniqid(rand()));
-        
-        $this->_objRequestParams = new stdClass();
+		srand((int)(microtime(true) * 1000000));
+		$this->_requestIdentifier = md5(uniqid(rand()));
+		$this->_objRequestParams = new stdClass();
 	}
 	
 	abstract protected function _prepare();
@@ -141,24 +168,31 @@ abstract class Mobilpay_Payment_Request_Abstract
 		return $objPmReq;
 	}
 	
-	static public function factoryFromEncrypted($envKey, $encData, $privateKeyFilePath, $privateKeyPassword = null)
+	static public function factoryFromEncrypted($envKey, $encData, $privateKeyFilePath, $privateKeyPassword = null, $cipher_algo = 'rc4', $iv = null)
 	{
 		$privateKey = null;
-		if($privateKeyPassword == null)
+		if ($privateKeyPassword == null)
 		{
-			$privateKey = @openssl_get_privatekey("file://{$privateKeyFilePath}");
+			$privateKey = @openssl_get_privatekey($privateKeyFilePath);
+			if ($privateKey === false)
+			{
+				$privateKey = @openssl_get_privatekey("file://{$privateKeyFilePath}");
+			}
 		}
 		else
 		{
-			$privateKey = @openssl_get_privatekey("file://{$privateKeyFilePath}", $privateKeyPassword);
+			$privateKey = @openssl_get_privatekey($privateKeyFilePath, $privateKeyPassword);
+			if ($privateKey === false)
+			{
+				$privateKey = @openssl_get_privatekey("file://{$privateKeyFilePath}", $privateKeyPassword);
+			}
 		}
-		if($privateKey === false)
-        {
-        	throw new Exception('Error loading private key', self::ERROR_CONFIRM_LOAD_PRIVATE_KEY);
-        }
-        
-        $srcData = base64_decode($encData);
-		if($srcData === false)
+		if ($privateKey === false)
+		{
+			throw new Exception('Error loading private key', self::ERROR_CONFIRM_LOAD_PRIVATE_KEY);
+		}
+		$srcData = base64_decode($encData);
+		if ($srcData === false)
 		{
 			@openssl_free_key($privateKey);
 			throw new Exception('Failed decoding data', self::ERROR_CONFIRM_FAILED_DECODING_DATA);
@@ -169,10 +203,20 @@ abstract class Mobilpay_Payment_Request_Abstract
 		{
 			throw new Exception('Failed decoding envelope key', self::ERROR_CONFIRM_FAILED_DECODING_ENVELOPE_KEY);
 		}
-		
+		$srcIv = base64_decode($iv);
+		if($srcIv === false)
+		{
+			throw new Exception('Failed decoding initialization vector', self::ERROR_CONFIRM_FAILED_DECODING_IV);
+		}
 		$data = null;
-		$cipher_algo = 'RC4';
-		$result = @openssl_open($srcData, $data, $srcEnvKey, $privateKey, $cipher_algo);
+		if(PHP_VERSION_ID >= 70000)
+		{
+			$result = @openssl_open($srcData, $data, $srcEnvKey, $privateKey, $cipher_algo, $srcIv);
+		}
+		else
+		{
+			$result = @openssl_open($srcData, $data, $srcEnvKey, $privateKey, $cipher_algo);
+		}
 		if($result === false)
 		{
 			throw new Exception('Failed decrypting data', self::ERROR_CONFIRM_FAILED_DECRYPT_DATA);
@@ -245,6 +289,15 @@ abstract class Mobilpay_Payment_Request_Abstract
 		{
 			throw new Exception('Mobilpay_Payment_Request_Sms::loadFromXml failed: signature is missing', self::ERROR_LOAD_FROM_XML_SIGNATURE_ELEM_MISSING);
 		}
+		$xmlAttr = $elem->attributes->getNamedItem('secretcode');
+		if ($xmlAttr == null || strlen((string) $xmlAttr->nodeValue) == 0)
+		{
+			$this->secretCode = '';
+		}
+		else
+		{
+			$this->secretCode = $xmlAttr->nodeValue;
+		}
 		$xmlElem = $elems->item(0);
 		$this->signature = $xmlElem->nodeValue;
 		
@@ -262,8 +315,21 @@ abstract class Mobilpay_Payment_Request_Abstract
 			$elems = $xmlElem->getElementsByTagName('confirm');
 			if($elems->length == 1)
 			{
-				$this->confirmUrl = $elems->item(0)->nodeValue; 
+				$this->confirmUrl = $elems->item(0)->nodeValue;
 			}
+			//check for overwritten cancel url
+			$elems = $xmlElem->getElementsByTagName('cancel');
+			if ($elems->length == 1)
+			{
+				$this->cancelUrl = $elems->item(0)->nodeValue;
+			}
+		}
+		
+		$elems = $elem->getElementsByTagName('ipn_cipher');
+		if($elems->length == 1)
+		{
+			$xmlElem = $elems->item(0);
+			$this->ipnCipher = $xmlElem->nodeValue;
 		}
 		
 		$this->params = array();
@@ -279,15 +345,28 @@ abstract class Mobilpay_Payment_Request_Abstract
 				{
 					continue;
 				}
-				$paramName = $elems->item(0)->nodeValue; 
+				$paramName = $elems->item(0)->nodeValue;
+				$what = '/(\[[0-9]*\])/i';
+				$with = '';
+				$paramName = preg_replace($what, $with, $paramName);
 	
 				$elems = $xmlParam->getElementsByTagName('value');
 				if($elems->length != 1)
 				{
 					continue;
 				}
-				
-				$this->params[$paramName] = urldecode($elems->item(0)->nodeValue); 
+				if (isset($this->params[$paramName]))
+				{
+					if (!is_array($this->params[$paramName]))
+					{
+						$this->params[$paramName] = array($this->params[$paramName]);
+					}
+					$this->params[$paramName][] = urldecode($elems->item(0)->nodeValue);
+				}
+				else
+				{
+					$this->params[$paramName] = urldecode($elems->item(0)->nodeValue);
+				}
 			}
 		}
 		
@@ -301,13 +380,17 @@ abstract class Mobilpay_Payment_Request_Abstract
 	
 	public function encrypt($x509FilePath)
 	{
-		$this->_prepare();
-		
-		$publicKey = openssl_pkey_get_public("file://{$x509FilePath}");
-		if($publicKey === false)
+		$publicKey = openssl_pkey_get_public($x509FilePath);
+		if ($publicKey === false)
 		{
-			$this->outEncData	= null;
-			$this->outEnvKey	= null;
+			$publicKey = openssl_pkey_get_public("file://{$x509FilePath}");
+		}
+		if ($publicKey === false)
+		{
+			$this->outEncData 	= null;
+			$this->outEnvKey 	= null;
+			$this->outCipher 	= null;
+			$this->outIv 		= null;
 			$errorMessage = "Error while loading X509 public key certificate! Reason:";
 			while(($errorString = openssl_error_string()))
 			{
@@ -315,16 +398,69 @@ abstract class Mobilpay_Payment_Request_Abstract
 			}
 			throw new Exception($errorMessage, self::ERROR_LOAD_X509_CERTIFICATE);
 		}
+		$publicKeys = array(
+			$publicKey
+		);
+		$encData 		= null;
+		$envKeys 		= null;
+		$cipher_algo 	= 'rc4';
+		$iv 			= null;
+		if(PHP_VERSION_ID >= 70000)
+		{
+			if(OPENSSL_VERSION_NUMBER > 0x10000000)
+			{
+				$cipher_algo = 'aes-256-cbc';
+			}	
+		}
+		else
+		{
+			if(OPENSSL_VERSION_NUMBER >= 0x30000000)
+			{
+				$this->outEncData 	= null;
+				$this->outEnvKey 	= null;
+				$this->outCipher 	= null;
+				$this->outIv 		= null;
+				$errorMessage 		= 'incompatible configuration PHP ' . PHP_VERSION . ' & ' . OPENSSL_VERSION_TEXT;
+				throw new Exception($errorMessage, self::ERROR_REQUIRED_CIPHER_NOT_AVAILABLE);
+			}
+		}
+		$opensslCipherMethods = openssl_get_cipher_methods();
+		if(in_array($cipher_algo, $opensslCipherMethods))
+		{
+		}
+		else if(in_array(strtoupper($cipher_algo), $opensslCipherMethods))
+		{
+			$cipher_algo = strtoupper($cipher_algo);
+		}
+		else
+		{
+			$this->outEncData 	= null;
+			$this->outEnvKey 	= null;
+			$this->outCipher 	= null;
+			$this->outIv 		= null;
+			$errorMessage 		= '`' . $cipher_algo . '` required cipher is not available';
+			throw new Exception($errorMessage, self::ERROR_REQUIRED_CIPHER_NOT_AVAILABLE);
+		}
+		if($this->ipnCipher === null)
+		{
+			$this->ipnCipher = $cipher_algo;
+		}
+		$this->_prepare();
 		$srcData = $this->_xmlDoc->saveXML();
-		$publicKeys	 = array($publicKey);
-		$encData 	 = null;
-		$envKeys 	 = null;
-		$cipher_algo = 'RC4';
-		$result 	 = openssl_seal($srcData, $encData, $envKeys, $publicKeys, $cipher_algo);
+		if(PHP_VERSION_ID >= 70000)
+		{
+			$result = openssl_seal($srcData, $encData, $envKeys, $publicKeys, $cipher_algo, $iv);
+		}
+		else
+		{
+			$result = openssl_seal($srcData, $encData, $envKeys, $publicKeys, $cipher_algo);
+		}
 		if($result === false)
 		{
-			$this->outEncData	= null;
-			$this->outEnvKey	= null;
+			$this->outEncData 	= null;
+			$this->outEnvKey 	= null;
+			$this->outCipher 	= null;
+			$this->outIv 		= null;
 			$errorMessage = "Error while encrypting data! Reason:";
 			while(($errorString = openssl_error_string()))
 			{
@@ -335,6 +471,8 @@ abstract class Mobilpay_Payment_Request_Abstract
 		
 		$this->outEncData 	= base64_encode($encData);
 		$this->outEnvKey 	= base64_encode($envKeys[0]);
+		$this->outCipher 	= $cipher_algo;
+		$this->outIv 		= (strlen($iv) > 0) ? base64_encode($iv) : '';
 	}
 
 	public function getEnvKey()
@@ -345,6 +483,32 @@ abstract class Mobilpay_Payment_Request_Abstract
 	public function getEncData()
 	{
 		return $this->outEncData;
+	}
+	
+	public function getCipher()
+	{
+		return $this->outCipher;
+	}
+	
+	/**
+	 * Return initialization vector seed used to encrypt data
+	 */
+	public function getIv()
+	{
+		return $this->outIv;
+	}
+	
+	/**
+	 *
+	 * Set the request identifier
+	 * @return
+	 */
+	public function setRequestIdentifierPrefix($prefix = null)
+	{
+		if (strpos($this->_requestIdentifier, $prefix) !== 0)
+		{
+			$this->_requestIdentifier = $prefix . $this->_requestIdentifier;
+		}
 	}
 	
 	public function getRequestIdentifier()
@@ -377,6 +541,32 @@ abstract class Mobilpay_Payment_Request_Abstract
     }
     public function __sleep()
     {
-        return array('_requestIdentifier','orderId','signature', 'returnUrl', 'confirmUrl', 'params');
+        return array(
+			'_requestIdentifier',
+			'_objRequestInfo',
+			'invoice',
+			'orderId',
+			'signature',
+			'returnUrl',
+			'confirmUrl',
+			'cancelUrl',
+			'params',
+			'reqInstallments',
+			'selectedInstallments',
+			'secretCode',
+			'paymentInstrument', 
+			'ipnCipher'
+		);
     }
+    public function toXml()
+	{
+		$this->_prepare();		
+		return $this->_xmlDoc->saveXML();
+	}
+
+    public function getXml()
+	{
+		$this->_prepare();
+		return $this->_xmlDoc;
+	}
 }
